6f739ca20dde8d4fa13aab8e7360e27170da432d
RengganaKurniawan <6182201071@student.unpar.ac.id>
2025-09-13 23:21:34 +0700

    add main part

--- src/pages/LandingPages.tsx
+++ src/pages/LandingPages.tsx
@@ -36,6 +36,7 @@ const LandingPage = () => {
    const Card = ({ title }: CardProps) => {
     return (
         <div className="flex flex-col items-center justify-center p-6 bg-[#343536] rounded-lg shadow-lg hover:shadow-xl transition-shadow cursor-pointer">
+            {/* DUMMY ICON */}
             <div className="w-16 h-16 rounded-full bg-yellow-400 flex items-center justify-center mb-4 text-black">
                 <span className="text-3xl font-bold text-gray-800">{title.charAt(0)}</span>
             </div>
@@ -49,32 +50,52 @@ const LandingPage = () => {
             <Header />
 
             <main className="container mx-auto p-8">
-                
-                {/* welcome section */}
-                <div className="mb-8">
-
-                    {/* DUMMY NAME */}
-                    <h2 className="text-3x1 font-bold">Welcome, User</h2>
-                    {/* DUMMY DATE */}
-                    <p className="text-gray-400 text-sm">Last sign in: 11/09/25</p>
+                {/* Top Section */}
+                <div className="mb-2">
+                    {/* Welcome, User */}
+                    <div>
+                        {/* DUMMY NAME */}
+                        <h2 className="text-3x1 font-bold">Welcome, User</h2>
+                    </div>
+                    
+                    <div className="flex items-center justify-between mb-12 text-sm">
+                        {/* DUMMY DATE */}
+                        <p className="text-gray-400 text-sm">Last sign in: 11/09/25</p>
+                        {/* row data section */}
+                        <div className="flex flex-wrap md:flexnowrap justify-end space-x-8">
+                            <div className="flex items-center space-x-2">
+                                <span><span className="font-semibold text-blue-400">87</span> Enterprise</span>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <span><span className="font-semibold text-yellow-400">30</span> Locations</span>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <span><span className="font-semibold text-green-400">46</span> Alarms</span>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <span><span className="font-semibold text-teal-400">15</span> total gNB</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                {/* row data section */}
-                <div className="flex items-center justify-end space-x-8 mb-12 text-sm">
-                    <div className="flex items-center space-x-2">
-                        <span><span className="font-semibold text-blue-400">87</span> Enterprise</span>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <span><span className="font-semibold text-yellow-400">30</span> Locations</span>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <span><span className="font-semibold text-green-400">46</span> Alarms</span>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <span><span className="font-semibold text-teal-400">15</span> total gNB</span>
-                    </div>
-                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
+                    <Card title="Vendor Agreement & Query" />
+                    <Card title="Vendor Management" />
+                    <Card title="Vendor Communication" />
+                    <Card title="Policy Configuration" />
+                    <Card title="User Management" />
+                    <Card title="Network Planning" />
+                    <Card title="Service Assurance" />
+                    <Card title="Inventory" />
+                    <Card title="Network Configuration" />
+                    <Card title="Service Flow Design" />
+                    <Card title="Reporting & Analytics" />
+                    <Card title="Notifications" />
+                </div>                
             </main>
+
+            <footer className="p-6" />
         </div>
     );
 };
