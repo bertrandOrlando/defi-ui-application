ae8728a6a47f9c7f97f9361fa10a883956b5ed19
stevenchay123 <6182201068@student.unpar.ac.id>
2025-10-14 17:23:56 +0700

    fix: remove import and route for deleted UserGroupManagement page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import ProtectedRoute from "./component/ProtectedRoute";
 import NotFoundPage from "./pages/NotFoundPage";
 import RootHandler from "./pages/RootHandler";
 import UserManagement from "./pages/UserManagement";
-import UserGroupManagement from "./pages/UserGroupManagement";
 import OutOfScope from "./pages/OutOfScope";
 
 function App() {
@@ -66,14 +65,7 @@ function App() {
           </ProtectedRoute>
         }
       />
-      <Route
-        path="/user-group-management"
-        element={
-          <ProtectedRoute>
-            <UserGroupManagement />
-          </ProtectedRoute>
-        }
-      />
+     
 
       <Route
         path="/network-planning"
